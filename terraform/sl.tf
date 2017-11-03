5a2b2378f4ec82e0c5af07e6e60e1f9835ec9f01
matekamoris <benoit.creau@chmod666.org>
2017-11-03 23:31:57 +0100

    Fixing terraform plan

--- terraform/sl.tf
+++ terraform/sl.tf
@@ -1,36 +1,55 @@
 provider "scaleway" {
-  region       = "par1"
+  region = "${var.region}"
 }
 
 data "scaleway_image" "ubuntu" {
-  architecture = "x86_64"
-  name         = "Ubuntu Zesty"
+  architecture = "${var.architecture}"
+  name         = "${var.image}"
+}
+
+resource "scaleway_ip" "public_ip" {
+  count = 1
 }
 
 resource "scaleway_server" "worker" {
-  count = 2
-  name  = "worker${count.index}"
-  image = "${data.scaleway_image.ubuntu.id}"
-  type  = "VC1S"
-  state  = "running"
+  count  = "${var.worker_instance_count}"
+  name   = "worker${count.index}"
+  image  = "${data.scaleway_image.ubuntu.id}"
+  type   = "${var.worker_instance_type}"
+  state  = "started"
+  tags   = ["workers"]
 
-  /* volume {
+  volume {
     size_in_gb = 50
     type       = "l_ssd"
-  } */
+  }
 }
 
 resource "scaleway_server" "master" {
-  count = 1 
-  name  = "master${count.index}"
-  image = "${data.scaleway_image.ubuntu.id}"
-  type  = "VC1S"
-  state  = "running"
+  count  = "${var.master_instance_count}"
+  name   = "master${count.index}"
+  image  = "${data.scaleway_image.ubuntu.id}"
+  type   = "${var.master_instance_type}"
+  state  = "started"
+  tags   = ["masters"]
+}
 
-  /* volume {
-    size_in_gb = 50
-    type       = "l_ssd"
-  } */
+resource "scaleway_server" "proxy0" {
+  count = 1
+  name  = "proxy0"
+  image = "${data.scaleway_image.ubuntu.id}"
+  type  = "${var.proxy_instance_type}"
+  public_ip  = "${element(scaleway_ip.public_ip.*.ip, count.index)}"
+  state  = "started"
+  tags  = ["proxy","primary"]
+}
+
+resource "scaleway_server" "proxy1" {
+  name  = "proxy1"
+  image = "${data.scaleway_image.ubuntu.id}"
+  type  = "${var.proxy_instance_type}"
+  state  = "started"
+  tags  = ["proxy","secondary"]
 }
 
 output "worker_private_ips" {
@@ -38,5 +57,17 @@ output "worker_private_ips" {
 }
 
 output "master_private_ips" {
-  value = ["${scaleway_server.worker.*.private_ips}"]
+  value = ["${scaleway_server.master.*.private_ips}"]
+}
+
+output "proxy0_private_ips" {
+  value = ["${scaleway_server.proxy1.*.private_ips}"]
+}
+
+output "proxy1_private_ips" {
+  value = ["${scaleway_server.proxy1.*.private_ips}"]
+}
+
+output "public_ip" {
+  value = ["${scaleway_server.proxy1.*.public_ip}"]
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  default = "par1"
+}
+
+variable "architecture" {
+  default = "x86_64"
+}
+
+variable "image" {
+  default = "Ubuntu Zesty"
+}
+
+variable "master_instance_type" {
+  default = "VC1S"
+}
+
+variable "master_instance_count" {
+  default = 3
+}
+
+variable "proxy_instance_type" {
+  default = "VC1S"
+}
+
+variable "worker_instance_type" {
+  default = "VC1M"
+}
+
+variable "worker_volume_size" {
+  default = 100
+}
+
+variable "worker_instance_count" {
+  default = 3
+}
